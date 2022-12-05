eebeb40c7fda4a8a4d7dff5136b8bcab071caca1
Valeria <valeria_korzun@mail.ru>
2022-12-05 15:05:09 +0300

    Возведение в степень

---
+++ Task_25/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= B; i++)
+    result *= A;
+Console.WriteLine($"Результат: {result}");
