2bbc1f5f5c2e1538964ebe400beb1dd887d9ea6e
Valeria <valeria_korzun@mail.ru>
2022-12-05 15:06:22 +0300

    Создание массива

---
+++ Task_29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов, 
+// запрашивает данные(элементы массива) у пользователя и выводит их на экран.
+
+Console.Clear();
+int [] array = new int [8];
+for (int i=0; i < array.Length; i ++)
+{
+    Console.Write($"Введите {i+1} элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write($"[{string.Join(", ", array)}]");
